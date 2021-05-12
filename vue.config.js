07addf8db4fd2a702ce69f69f748680930d22651
unknown <email@example.com>
2021-05-12 16:22:16 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -24,10 +24,10 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/vue_cli',
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'vue_cli',
+  lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
     port: port,
